73d5cbb4a52df93e41a86618389cd053deddbc92
Takaki Wakuda <taka130102@hotmail.co.jp>
2021-11-22 13:40:53 +0900

    Add CallerArgumentExpressionAttribute

---
+++ CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,21 @@
+﻿// URL: https://github.com/takakiwakuda/CSharp-Polyfill
+
+#if !NETCOREAPP3_0_OR_GREATER
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    [DebuggerNonUserCode]
+    [ExcludeFromCodeCoverage]
+    [SuppressMessage("csharp", "IDE0060")]
+    internal sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        internal CallerArgumentExpressionAttribute(string parameterName)
+        {
+        }
+    }
+}
+#endif
